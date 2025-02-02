806d9df5024a949551c4a96b445f5924b397092f
Nikolay Slavov <119948467+NikolaySlavov-96@users.noreply.github.com>
2025-02-02 20:20:40 +0200

    Update search.js

--- src/data/search.js
+++ src/data/search.js
@@ -1,7 +1,8 @@
 import * as api from './api.js';
 
 const endPoints = {
-    'search': (query) => `/data/fruits?where=name%20LIKE%20%22${query}%22`
+    // 'search': (query) => `/data/fruits?where=name%20LIKE%20%22${query}%22`
+    'search': (query) => `/classes/Fruit?where=${encodeURIComponent(`{"nameFruit":{"$regex": "^${query}"}}`)}`
 }
 
 export async function searchEngine(query) {
